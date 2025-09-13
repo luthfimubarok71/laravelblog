29f1c629f8772727d907da06023f5cfcf3cd0a57
luthfimubarok71 <luthfimubarok71@gmail.com>
2025-09-13 20:30:37 +0700

    feat (route): tambah route baru untuk posts

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,29 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', ['title' => 'Home Page']);
+});
+
+Route::get('/posts', function () {
+    $posts = Post::latest()->filter(request(['keyword', 'category', 'author']))->paginate(8)->withQueryString();
+
+    return view('posts', ['title' => 'Blog', 'posts' => $posts]);
+});
+
+Route::get('/posts/{post:slug}', function (Post $post) {
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
+Route::get('/about', function () {
+    return view('about', ['title' => 'About']);
+});
+
+Route::get('/contact', function () {
+    return view('contact', ['title' => 'Contact']);
 });
 
 Route::get('/dashboard', function () {
@@ -17,4 +36,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
