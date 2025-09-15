a6c2501910e1e33b528b539155f5efe4bc10c8c5
luthfimubarok71 <luthfimubarok71@gmail.com>
2025-09-15 23:41:31 +0700

    chore (fillable): memberikan akses penambahan field pada model post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,7 +11,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Post extends Model
 {
     use HasFactory;
-    protected $fillable = ['title', 'slug', 'author', 'body'];
+    protected $fillable = ['title', 'slug', 'author_id', 'category_id', 'body'];
     // protected $guarded = ['id'];
     protected $with = ['category', 'author'];
 
